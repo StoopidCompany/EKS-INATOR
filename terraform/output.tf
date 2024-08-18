7f29f4329d73355518b8b106f0fc0eb97830f726
Jason Anton <rev.jt.anton@gmail.com>
2024-08-18 16:22:00 -0400

    fix: exporting the node_group arn (#6)
    Resolves #5

--- terraform/output.tf
+++ terraform/output.tf
@@ -64,8 +64,13 @@ output "eks_cluster_oidc_issuer_url" {
 }
 
 output "eks_cluster_iam_role_arn" {
-  description = "The IAM role ARN for the node group for the cluster"
+  description = "The IAM role ARN for the cluster"
   value       = module.eks.eks_cluster_iam_role_arn
+}
+
+output "eks_cluster_node_group_role_arn" {
+  description = "The IAM role ARN for the node group for the cluster"
+  value       = module.eks.eks_managed_node_group_iam_role_arns["general"]
 }
 
 ################################################################################
